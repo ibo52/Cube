c1d15ffc294a1bab3ab82762de0bcbcecbae825c
ibo52 <halilibrahimmut@hotmail.com>
2022-04-21 15:36:33 +0300

    initial commit-2D cube

---
+++ cube.py
@@ -0,0 +1,47 @@
+"""halil ibrahim mut
+Performing actions on the cube according
+to the matrix operations learned from
+Computer Graphics
+-------------------------------------
+Computer Graphics deslerinden öğrenilen
+matris işlemlerine göre küpte eylemler
+gerçekleştirmek
+"""
+
+#required functions and stuffs are on settings.py file
+from settings import *
+
+pygame.init()
+
+#define the points of cube that its centers on origin
+cube=[-1,1,
+      1,1,
+      1,-1,
+      -1,-1]
+
+
+cube=size(cube,40)      #rescaling for bigger cube
+
+cube=translate(cube,150,150)#translate points to center of window
+
+def run():
+    global cube
+    global clock
+    done=False
+    while not done:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                done=True
+            elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_SPACE:
+                    print("space")
+                    #cube=rotate(cube,10)
+
+        ekran.fill(WHITE)
+        draw_cube(cube)
+        cube=rotate(cube,1)
+        pygame.display.flip()
+        clock.tick(60)
+run()
+pygame.quit()
+exit()
---
+++ settings.py
@@ -0,0 +1,97 @@
+"""
+Halil Ibrahim Mut
+settings for CG matrix operations
+"""
+import pygame
+from math import cos, sin, radians
+
+FPS = 60
+
+# ---- window settings ----
+RESOLUTION = (300, 300)  # window size
+WINDOW_NAME = "hz ibo; CG 2d MATRIX deneme"
+
+clock = pygame.time.Clock()
+ekran = pygame.display.set_mode(RESOLUTION)
+pygame.display.set_caption(WINDOW_NAME)
+# --------------------------
+
+# ---- colours ----
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE=(0,0,255)
+RED=(255,0,0)
+GREEN=(0,255,0)
+# -----------------
+
+def draw_cube(array,color_array=[BLACK]):
+    color_len=len(color_array)
+
+    arr_size=len(array)
+    for idx in range(0,arr_size,2):
+        pygame.draw.line(ekran, color_array[idx%color_len], (array[idx], array[idx+1]),
+                         (array[(idx+2)%arr_size], array[(idx+3)%arr_size]), 5)
+
+    """ OLD : deprecated
+    pygame.draw.line(ekran, BLACK, (array[0], array[1]), (array[2], array[3]), 5)
+    pygame.draw.line(ekran, BLACK, (array[2], array[3]), (array[4], array[5]), 5)
+    pygame.draw.line(ekran, BLACK, (array[4], array[5]), (array[6], array[7]), 5)
+    pygame.draw.line(ekran, BLACK, (array[6], array[7]), (array[0], array[1]), 5)
+    """
+
+# resize the points on an array
+def size(array, coefficient):
+    l = array.copy()
+    for idx in range(len(array)):
+        l[idx] *= coefficient
+
+    return l
+
+
+# move/displace the points on an array
+
+def translate2(array, x=0, y=0):
+    """ old and wrong function.Deprecated """
+    l = array.copy()
+    for idx in range(len(array)):
+        l[idx] += x
+        # l[idx+1]=+
+
+    return l
+
+
+def translate(array, x=0, y=0):
+    """-_-"""
+    l = array.copy()
+    idx = 0
+    while (idx + 1 < len(array)):
+        l[idx] = x + array[idx]
+        l[idx + 1] = y + array[idx + 1]
+        idx += 2
+
+    return l
+
+
+# --------------------------
+
+# rotate the points on an array on their own center
+def rotate(array, theta):
+    """rotate x-y planes on array according to theta"""
+    theta = radians(theta)
+    rot_matrix = [cos(theta), -sin(theta),
+                  sin(theta), cos(theta)]
+
+    # IMPORTANT:firstly translate to origin
+    # to rotate around their OWN center
+    array = translate(array, -150, -150)
+
+    idx = 0
+    rotated = []
+    while (idx + 1 < len(array)):
+        x = rot_matrix[0] * array[idx] + rot_matrix[1] * array[idx + 1]
+        y = rot_matrix[2] * array[idx] + rot_matrix[3] * array[idx + 1]
+        rotated += x, y
+        idx += 2
+    # translate to original positions
+    rotated = translate(rotated, 150, 150)
+    return rotated
