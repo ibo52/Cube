e822c5bf00ae440ccc5d772c6b9f383b03d40ea6
Halil Ibrahim Mut <102026175+ibo52@users.noreply.github.com>
2022-04-22 14:39:09 +0300

    Update cube.py

--- cube.py
+++ cube.py
@@ -18,14 +18,23 @@ cube=[-1,1,
       1,1,
       1,-1,
       -1,-1]
-
-
 cube=size(cube,40)      #rescaling for bigger cube
+cube=translate(cube,100,RESOLUTION[1]//2 -40)#RESOLUTION[1]//2)#translate points to center of window
 
-cube=translate(cube,150,150)#translate points to center of window
+cube3d=[-1,-1,-1,
+        1,-1,-1,
+        1,1,-1,
+        -1,1,-1,
+        -1,-1,1,
+        1,-1,1,
+        1,1,1,
+        -1,1,1]
+cube3d=size(cube3d,40)#rescaling for bigger 3d cube
+cube3d=trans3d(cube3d,240,RESOLUTION[1]//2 -40,150)#translate points to center of window
 
 def run():
     global cube
+    global cube3d
     global clock
     done=False
     while not done:
@@ -38,10 +47,13 @@ def run():
                     #cube=rotate(cube,10)
 
         ekran.fill(WHITE)
-        draw_cube(cube)
-        cube=rotate(cube,1)
+        draw_cube(cube,[RED,GREEN,BLUE,BLACK])#delete 2nd argument if you want one color
+        cube=rotate(cube,theta_z=1)
+
+        draw3d(cube3d, [RED, GREEN, BLUE, BLACK])
+        cube3d = rotate3d(cube3d, theta_x=1, theta_y=1, theta_z=1)
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(FPS)
 run()
 pygame.quit()
 exit()
